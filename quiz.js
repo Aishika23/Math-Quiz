78d08aaaf52e1e80ee1b4fcf7f229b4ad158e0da
Aishika23 <100014326+Aishika23@users.noreply.github.com>
2022-06-12 11:49:35 +0530

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,3 +1,19 @@
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("pl1").innerHTML = player1_name + ": ";
+document.getElementById("pl2").innerHTML = player2_name + ": ";
+
+document.getElementById("player1_score").innerHTML = player1_score + ": ";
+document.getElementById("player2_score").innerHTML = player2_score + ": ";
+
+document.getElementById("player_question").innerHTML = "Question turn - " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer turn - " + player2_name;
+
+
 function send() 
 {
     number1 = document.getElementById("number1").value;
@@ -10,24 +26,50 @@ function send()
     row = question_number + input_box + check_button ;
     document.getElementById("output").innerHTML = row;
     
+    document.getElementById("number1").innerHTML = "";
+    document.getElementById("number2").innerHTML = "";
 
-    number1 = ""
-    number2 = ""
-    
 }
-player1_name = localStorage.getItem("player1_name");
-player2_name = localStorage.getItem("player2_name");
 
-player1_score = 0;
-player2_score = 0;
+question_turn = "player1";
+answer_turn = "player2";
 
-document.getElementById("player1_name").innerHTML = player1_name + ": ";
-document.getElementById("player2_name").innerHTML = player2_name + ": ";
-
-document.getElementById("player1_score").innerHTML = player1_score + ": ";
-document.getElementById("player2_score").innerHTML = player2_score + ": ";
-
-pl1_display = "Question turn - " + player1_name;
-pl2_display = "Answer turn - " + player2_name;
-row2 = pl1_display + pl2_display ;
-document.getElementById("turn").innerHTML = row2;
+function check() 
+{
+    question_turn = "player1";
+    answer_turn = "player2";
+    get_answer = document.getElementById("input_check_box").value;
+    if (get_answer == actual_answer) 
+    {
+        if (answer_turn == "player1") 
+        {
+            update_player1_score = player1_score + 1;
+            document.getElementById("player1_score").innerHTML = update_player1_score;
+        } 
+        else 
+        {
+            update_player2_score = player2_score + 1;
+            document.getElementById("player2_score").innerHTML = update_player2_score;
+        }
+    }
+    if (question_turn == "player1") 
+    {
+        question_turn = "player2"
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+    } 
+    else 
+    {
+        question_turn = "player1"
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+    }
+    if (answer_turn == "player1") 
+    {
+        answer_turn = "player2"
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+    } 
+    else 
+    {
+        answer_turn = "player1"
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
+    }
+}
